30e65d844bd53c34b6b53a96e4708f24f4846633
SachaWildCode <sacha.loumachi@gmail.com>
2023-04-20 10:48:50 +0200

    first commit

--- src/Fighter.js
+++ src/Fighter.js
@@ -1,46 +1,40 @@
-const MAX_LIFE = 100
+const MAX_LIFE = 100;
 
 class Fighter {
-  constructor(name, strength, dexterity, image) {
+  constructor(name, strength, dexterity, image, x, y) {
     this.name = name;
     this.strength = strength;
     this.dexterity = dexterity;
     this.life = MAX_LIFE;
     this.image = image;
-    this.weapon = null;
-    this.shield = null;
+    this.x = x;
+    this.y = y;
   }
 
   /**
    * Launch a strike
    * @param Fighter defender
    */
- fight(defender) {
+  fight(defender) {
     let attackPoints = this.getRandomInt(this.getDamage());
-    let damages = Math.max(attackPoints - defender.getDefense(), 0)
+    let damages = Math.max(attackPoints - defender.getDefense(), 0);
     defender.life = Math.max(defender.life - damages, 0);
   }
-
 
   /**
    * Calculate the value of the defense
    * @returns
    */
   getDamage() {
-    return this.weapon ?
-      this.strength + this.weapon.damage :
-      this.strength;
+    return this.strength;
   }
-
 
   /**
    * Calculate the value of the attack
    * @returns
    */
   getDefense() {
-    return this.shield ?
-      this.dexterity + this.shield.protection :
-      this.dexterity;
+    return this.dexterity;
   }
 
   /**
@@ -52,13 +46,11 @@ class Fighter {
     return 1 + Math.floor(Math.random() * max);
   }
 
-
   /**
-  * Check if the fighters is still alive
-  * @returns Boolean
-  */
+   * Check if the fighters is still alive
+   * @returns Boolean
+   */
   isAlive() {
-    return this.life > 0
+    return this.life > 0;
   }
 }
-
